663bd5c520c54ab55137414fd2fcdca801088818
Suha Mukadam <mukadamsuha006@gmail.com>
2022-03-11 17:35:17 +0530

    reverse

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = '';
+}
+
+import {Pipe, PipeTransform} from '@angular/core';
+
+
+
+    @Pipe({name: 'reverse'})
+    export class ReversePipe implements PipeTransform {
+        transform(value: string): string {
+            return value.split('').reverse().join('');
+        }
+    }
+    
+    
